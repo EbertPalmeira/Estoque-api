f497f1c2d1940fedf8b8d33762a6fccd17749a11
Ebert Palmeira <eberttpalmerass@gmail.com>
2026-08-19 10:30:17 -0300

    Corrige permissões e atualiza código

--- src/main/java/com/estoque/api/Principal.java
+++ src/main/java/com/estoque/api/Principal.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Principal {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Principal.class, args);
+
+
+
+
 	}
 
 }
